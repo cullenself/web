04269dbf0b7cd89f8e35bb43ad046bc01dcab86b
Cullen Self <cullen.cs@gmail.com>
2016-11-27 00:37:19 -0600

    Restructured and fixed create.sh to work with global Markdown.pl

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -f www/index.html
 cat src/head.html > www/index.html
-perl Markdown.pl src/content.md >> www/index.html
+Markdown.pl src/content.md >> www/index.html
 cat src/tail.html >> www/index.html
